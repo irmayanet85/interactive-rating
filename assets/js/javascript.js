1c3e3477c675537ce0d422d64e92af66baeb4a65
Irma Cabrera <irmayanet85@gmail.com>
2023-04-25 09:54:19 -0400

    optimizing code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,16 +1,16 @@
 /*jshint esversion: 6 */ 
 function selectFeedback(){
 
-    const listoptions = document.querySelectorAll('.option');
+    const listOptions = document.querySelectorAll('.option');
     
-    listoptions.forEach(option => {
+    listOptions.forEach(option => {
         option.addEventListener('click', event => {
             let element = event.target;
             if(document.querySelector('.selected'))
             {
-                let element_before = document.querySelector('.selected');
-                element_before.classList.remove('selected');
-                element_before.classList.add('hover');
+                let elementBefore = document.querySelector('.selected');
+                elementBefore.classList.remove('selected');
+                elementBefore.classList.add('hover');
             }
             element.classList.add('selected');
             element.classList.remove('hover');
@@ -20,40 +20,16 @@ function selectFeedback(){
     });
 }
 
-function createCardThank(value){
-   let template = document.querySelector('main');
+function showCardThank(value){
+   let value_feedback = document.querySelector('#value-feedback');
+   let card_Thank = document.querySelector('.card-thank');
+   value_feedback.value=value;
+   card_Thank.classList.remove('hidden');
+}
 
-   const elementsHtml = ` <div class="card-thank" >
-
-                       
-
-                        <div class="image-thank">
-                            <img src="./assets/images/illustration-thank-you.svg" alt="Ilustration thank you" style="margin: 0px auto;">
-                        </div>
-
-                        <div class="text-selected">
-
-                            <p>You selected <span> ${value} </span> out of 5</p>
-                        </div>
-
-                        <div class="title">
-                            <h1>Thank you!</h1>
-
-                        </div>
-
-                        <div class="description">
-                            <p>
-
-                            We appreciate you taking the time to give a rating. If you ever need more support, 
-                            don’t hesitate to get in touch!
-                            </p>
-
-                        </div>
-
-                    </div>`;
-
-    template.innerHTML = elementsHtml;
-
+function hiddenCardFeedBack(){
+  const card_FeedBack = document.querySelector('.card-feedback');
+  card_FeedBack.classList.add('hidden');
 }
 
 function sendFeedback(){
@@ -65,7 +41,10 @@ function sendFeedback(){
         if(selected){
             evaluation = selected.value;
         
-            if (evaluation >= 1 && evaluation <= 5) { createCardThank(evaluation);}
+            if (evaluation >= 1 && evaluation <= 5) { 
+                hiddenCardFeedBack();
+                showCardThank(evaluation);
+            }
             else { console.log('You must select a valid assessment');}
         }
         else { alert('You must select an evaluation'); }
